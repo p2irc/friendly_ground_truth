a583e4c61fa6ae43a11d95b774a69b46f27812ef
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2020-03-04 16:40:15 -0600

    Add a simple test to configure CI with

---
+++ tests/model/test_model.py
@@ -0,0 +1,21 @@
+"""
+File Name: test_model.py
+
+Authors: Kyle Seidenthal
+
+Date: 04-03-2020
+
+Description: Unit Testing for Model
+
+"""
+
+import pytest
+from friendly_ground_truth.model.model import Image, Patch
+
+class TestImage:
+    """
+    Tests pertaining to the Image class
+    """
+
+    def test_load_image(self):
+        assert 1 == 1
