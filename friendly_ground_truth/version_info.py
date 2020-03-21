48c098803a776abbb384f50471283bf4824374f6
Kyle Seidenthal <25314187+KyleS22@users.noreply.github.com>
2020-03-21 02:44:30 -0600

    Update version_info.py

--- friendly_ground_truth/version_info.py
+++ friendly_ground_truth/version_info.py
@@ -16,8 +16,8 @@ class VersionInfo():
 
     def __init__(self):
         self.VERSION_MAJOR = 0
-        self.VERSION_MINOR = 0
-        self.VERSION_PATCH = 2
+        self.VERSION_MINOR = 1
+        self.VERSION_PATCH = 0
 
     def get_version_string(self):
         return "v" + str(self.VERSION_MAJOR) + '.' +\
