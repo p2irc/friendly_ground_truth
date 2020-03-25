29c7ed8b378d3e60128164f7cbd0d74dd0e2e6d8
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2020-03-25 16:40:51 -0600

    Fix error in flood remove tool

--- friendly_ground_truth/version_info.py
+++ friendly_ground_truth/version_info.py
@@ -17,7 +17,7 @@ class VersionInfo():
     def __init__(self):
         self.VERSION_MAJOR = 0
         self.VERSION_MINOR = 3
-        self.VERSION_PATCH = 0
+        self.VERSION_PATCH = 1
 
     def get_version_string(self):
         return "v" + str(self.VERSION_MAJOR) + '.' +\
