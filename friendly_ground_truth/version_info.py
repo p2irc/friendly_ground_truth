eda44d15b40404d4c368e9bb7c8aa2e14a64c25f
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2020-04-13 12:04:52 -0600

    Update to version 4.0

--- friendly_ground_truth/version_info.py
+++ friendly_ground_truth/version_info.py
@@ -16,8 +16,8 @@ class VersionInfo():
 
     def __init__(self):
         self.VERSION_MAJOR = 0
-        self.VERSION_MINOR = 3
-        self.VERSION_PATCH = 3
+        self.VERSION_MINOR = 4
+        self.VERSION_PATCH = 0
 
     def get_version_string(self):
         return "v" + str(self.VERSION_MAJOR) + '.' +\
