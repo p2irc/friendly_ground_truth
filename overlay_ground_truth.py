7078c49a2c5de359e09c5b45e03fdacc558346bb
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2020-04-14 15:18:46 -0600

    Add tools to mark important landmarks

---
+++ overlay_ground_truth.py
@@ -0,0 +1,54 @@
+"""
+File Name: overlay_ground_truth.py
+
+Authors: Kyle Seidenthal
+
+Date: 14-04-2020
+
+Description: Script to display the overlay of the image and the ground truth
+
+"""
+
+import sys
+import skimage.io as io
+import skimage.segmentation as segmentation
+import skimage.color as colour
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main(image_path, landmarks_path, mask_path):
+    """
+    Run the script using the given image and ground truth
+
+    :param image_path: The path to the image
+    :param landmarks_path: The path to the numpy labelling file
+    :param mask_path: The path to the binary segmentation mask
+    :returns: None
+    """
+    img = io.imread(image_path)
+    landmarks = np.load(landmarks_path)
+    mask = io.imread(mask_path)
+
+    overlay_img = colour.label2rgb(landmarks, img, bg_label=0,
+                                   colors=['red', 'green', 'blue'])
+
+    boundary_img = segmentation.mark_boundaries(overlay_img, mask)
+
+    plt.imshow(boundary_img)
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 4:
+        print("Usage: python overlay_ground_truth.py path_to_img "
+              "path_to_landmarks path_to_mask")
+
+        sys.exit(0)
+
+    image_path = sys.argv[1]
+    landmark_path = sys.argv[2]
+    mask_path = sys.argv[3]
+
+    main(image_path, landmark_path, mask_path)
