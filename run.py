cce02b558f0acbb7f8815c25ca264f79325c6233
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2020-05-06 12:22:40 -0600

    Change back to a single windows exe

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ Description: The entry point for the application
 """
 
 from tkinter import Tk
+import tkinter as tk
 from PIL import ImageTk as itk
 from PIL import Image
 from io import BytesIO
@@ -17,6 +18,10 @@ from io import BytesIO
 import logging
 import sys
 import base64
+import os
+import shutil
+
+from tkinter import messagebox
 
 from friendly_ground_truth.controller.controller import Controller
 from friendly_ground_truth.view.icons.icon_strings import fgt_favicon
@@ -49,6 +54,82 @@ logger.addHandler(ch)
 
 
 if __name__ == '__main__':
+
+    if sys.platform == 'win32':
+
+        home = os.path.expanduser("~")
+
+        tools = os.path.join(home, "Tools")
+
+        if os.getcwd() != tools:
+
+            root = tk.Tk()
+
+            def exit():
+                root.destroy()
+
+            def install():
+                EXE = "./friendly_gt.exe"
+
+                if sys.platform != 'win32':
+                    sys.exit(0)
+
+                from win32com.client import Dispatch
+
+                home = os.path.expanduser("~")
+
+                tools = os.path.join(home, "Tools")
+
+                if not os.path.exists(tools):
+                    os.mkdir(tools)
+
+                program_path = os.path.join(tools, 'friendly_gt.exe')
+
+                shutil.copy(EXE, program_path)
+
+                path = "AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
+
+                path = os.path.join(home, path)
+
+                if not os.path.exists(path):
+                    messagebox.showinfo(title="Something Went Wrong",
+                                        message="Sorry, there was an issue"
+                                        " creating the shortcut.")
+                    sys.exit(0)
+
+                path = os.path.join(path, "Friendly Ground Truth.lnk")
+                target = program_path
+                wDir = tools
+                icon = program_path
+
+                shell = Dispatch('WScript.Shell')
+                shortcut = shell.CreateShortcut(path)
+                shortcut.Targetpath = target
+                shortcut.WorkingDirectory = wDir
+                shortcut.IconLocation = icon
+                shortcut.save()
+
+                messagebox.showinfo(title="Success!",
+                                    message="Application Installed!")
+                root.destroy()
+
+            label_text = "Would you like to install a\n" +\
+                         "start menu shortcut for Friendly Ground Truth?"
+
+            label = tk.Label(root, text=label_text)
+
+            button_panel = tk.Frame(root)
+
+            ok_button = tk.Button(button_panel, text="Yes", command=install)
+            cancel_button = tk.Button(button_panel, text="No", command=exit)
+
+            ok_button.pack(side='right')
+            cancel_button.pack(side='left')
+
+            label.pack()
+            button_panel.pack(fill='both')
+
+            root.mainloop()
 
     icon_data = base64.b64decode(fgt_favicon)
     icon_data = Image.open(BytesIO(icon_data))
