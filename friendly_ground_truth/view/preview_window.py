2acbfd3ea80452666e32b7cbd66d85d3bf843ec8
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2020-06-05 15:07:16 -0600

    Add scrollable preview window, Fixes #227

--- friendly_ground_truth/view/preview_window.py
+++ friendly_ground_truth/view/preview_window.py
@@ -12,7 +12,7 @@ Description: An image mask preview window.
 import tkinter as tk
 from tkinter import ttk
 
-from PIL import Image, ImageTk as itk
+from friendly_ground_truth.view.fgt_canvas import ScrollableImageCanvas
 
 
 class PreviewWindow(tk.Toplevel):
@@ -26,13 +26,10 @@ class PreviewWindow(tk.Toplevel):
     def __init__(self, img, controller, style):
         self._base = tk.Toplevel()
         self._base.title("Preview")
+        self._base.geometry("1000x800+200+200")
 
         self._frame = ttk.Frame(self._base)
 
-        self._canvas_size = img.shape[0]//2, img.shape[1]//2
-
-        self._base.minsize(width=self._canvas_size[1] + 50,
-                           height=self._canvas_size[0] + 50)
         self.img = img
         self.controller = controller
 
@@ -49,16 +46,18 @@ class PreviewWindow(tk.Toplevel):
 
         self._cancel_button.pack(side='left')
 
-        self._button_panel.pack(side='top', fill='both')
+        self._button_panel.grid(row=0, column=0, sticky="NEW")
+        self._canvas = ScrollableImageCanvas(self._frame, self.img, self,
+                                             style)
 
-        self._canvas = tk.Canvas(self._frame)
+        self._canvas.set_zoom(-5)
 
-        background = style.lookup("Canvas.TFrame", "background")
-        self._canvas.config(background=background)
+        self._canvas.grid(row=1, column=0, sticky="NSEW")
+        self._frame.grid_columnconfigure(0, weight=1)
+        self._frame.grid_rowconfigure(0, weight=0)
+        self._frame.grid_rowconfigure(1, weight=1)
 
-        self._canvas.pack(fill='both', expand='yes')
         self._frame.pack(fill='both', expand=True)
-        self._show_image()
 
     def _on_save(self):
         """
@@ -87,30 +86,3 @@ class PreviewWindow(tk.Toplevel):
             The window is destroyed.
         """
         self._base.destroy()
-
-    def _show_image(self):
-        """
-        Display the image on the canvas.
-
-
-        Returns:
-            None
-
-        Postconditions:
-            The image is displayed on the canvas.
-        """
-        image = Image.fromarray(self.img)
-
-        canvas_h = self._canvas_size[0]
-        canvas_w = self._canvas_size[1]
-
-        size = (canvas_w, canvas_h)
-        image = image.resize(size)
-        self._display_img = itk.PhotoImage(image=image)
-
-        x, y = 0, 0  # self.image_x, self.image_y
-
-        self._image_id = self._canvas.create_image(x, y, anchor="nw",
-                                                   image=self._display_img)
-        self._canvas.image = self._display_img
-        self._canvas.pack()
