cc8faac2fd83c01dec0553bcb7fa706bd4a8349e
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2020-07-03 16:55:42 -0600

    Update to version 0.12.0

--- friendly_ground_truth/version_info.py
+++ friendly_ground_truth/version_info.py
@@ -29,8 +29,8 @@ class VersionInfo():
 
     def __init__(self):
         self.VERSION_MAJOR = 0
-        self.VERSION_MINOR = 11
-        self.VERSION_PATCH = 2
+        self.VERSION_MINOR = 12
+        self.VERSION_PATCH = 0
 
     def get_version_string(self):
         """
